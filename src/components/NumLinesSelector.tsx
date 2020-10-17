fdf11a91172bb52735fb0f0f02c70fccf95d42ab
Heyden2019 <master.kazya@yandex.ru>
2020-10-17 21:07:28 +0300

    fix text

--- src/components/NumLinesSelector.tsx
+++ src/components/NumLinesSelector.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NumLinesSelector = ({selectedNumLines, setSelectedNumLines}) => {
     return (
         <div>
-            <p style={{color: 'white', marginBottom: 0}}>Joining with last (___) dots:</p>
+            <p style={{color: 'white', marginBottom: 0}}>Joining with previous (___) dots:</p>
             {[0,1,2,3,4,5].map(btnNum => (
                 <button key={btnNum}
                         className={`num-lines-selector ${btnNum === selectedNumLines ? "active" : ""}`}
